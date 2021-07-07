218d0f65d1e36757b70aa4ca17f7a7a4d9956d7a
omer358 <omermaki358@gmail.com>
2021-07-07 16:15:58 +0200

    Hight and Low problem

---
+++ HighAndLow.py
@@ -0,0 +1,26 @@
+"""
+7KYU
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Example:
+    high_and_low("1 2 3 4 5")  # return "5 1"
+    high_and_low("1 2 -3 4 5") # return "5 -3"
+    high_and_low("1 9 3 4 -5") # return "9 -5"
+
+Notes:
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.
+"""
+
+
+def high_and_low(numbers):
+    num = numbers.split()
+    for i in range(len(num)):
+        num[i] = int(num[i])
+
+    my_list = [max(num), min(num)]
+    return " ".join(map(str, my_list))
+
+
+print(high_and_low('1 2 34 5 6 3 4 6'))
--- main.py
+++ main.py
@@ -5,7 +5,9 @@
 
 
 def print_hi(name):
-    import GravityFlip
+    import HighAndLow
+
+
 # Use a breakpoint in the code line below to debug your script.
 # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
